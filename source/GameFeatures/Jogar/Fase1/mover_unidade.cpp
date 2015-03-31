a5c13f040d9e0aecafb6514bcd566105fbc46d85
João Paulo Ferreira <iamferreirajp@gmail.com>
2015-03-31 02:18:24 -0300

    Applied stylesheet design on mover_unidade.cpp and verifica_hexagono.cpp

--- source/GameFeatures/Jogar/Fase1/mover_unidade.cpp
+++ source/GameFeatures/Jogar/Fase1/mover_unidade.cpp
@@ -6,19 +6,15 @@
 #include "../../../../include/loop.h"
 #include "../../../../include/gamefeatures.h"
 
-
-void mover_soldado(SDL_Surface *screen, int i, int x, int y){
-    //for(int h = unidades_vermelhas[i]->x, w = unidades_vermelhas[i]->y;h>x; h = h-25){
-    //cout << "entrei for com h = " << h << endl;
-    //cout << "entrei com w = " << w << endl;
-    int posi = unidades_vermelhas[i]->x;
-    for(int j = posi;j<x;j++){
-    SDL_Rect pos = {j, y};
-    unidades_vermelhas[i]->x = j;
-    unidades_vermelhas[i]->y = y;
-    SDL_BlitSurface(unidades_vermelhas[i]->nome, NULL, screen, &pos);
-    SDL_Flip(screen);
-    }
-
-    //}
+/* allow the soldiers to be moved */
+void mover_soldado(SDL_Surface *screen, int i, int x, int y)
+{
+	int posi = unidades_vermelhas[i]->x;
+	for (int j = posi;j<x;j++) {
+		SDL_Rect pos = {j, y};
+		unidades_vermelhas[i]->x = j;
+		unidades_vermelhas[i]->y = y;
+		SDL_BlitSurface(unidades_vermelhas[i]->nome, NULL, screen, &pos);
+		SDL_Flip(screen);
+	}
 }
--- source/GameFeatures/Jogar/Fase1/verifica_hexagono.cpp
+++ source/GameFeatures/Jogar/Fase1/verifica_hexagono.cpp
@@ -6,108 +6,113 @@
 #include "../../../../include/video.h"
 #include "../../../../include/loop.h"
 #include "../../../../include/gamefeatures.h"
-#include<math.h>
+#include <math.h>
 
 using namespace std;
 
+/* verifies the hexagon models */
+int verifica_hexagono(int x, int y)
+{
+	float vx, vy;
+	float re=0;
+	float x_f = (float) x;
+	float y_f = (float) y;
 
-int verifica_hexagono(int x, int y){
+	for (unsigned int i=0;i<hexagonos.size();i++) {
+		for (unsigned int j=0;j<hexagonos[i].size();j++) {
+			vx = abs(x_f - hexagonos[i][j]->centro_x);
+			vy = abs(y_f - hexagonos[i][j]->centro_y);
+			re = (vx*vx)+(vy*vy);
+			re = sqrt(re);
+			if (re <= 20) {
+				hex_selecao->i = i;
+				hex_selecao->j = j;
+				return 1;
+			}
+			else {
+				/* nothing to do */
+			}
+		}
+	}
+return 0;
+}
 
-    float vx, vy;
-    float re=0;
-    float x_f = (float) x;
-    float y_f = (float) y;
+/* verifies if theres a unit */
+int possui_unidade(string color)
+{
+	if (hexagonos[hex_selecao->i][hex_selecao->j]->contem_unidade) {
+		if (!hexagonos[hex_selecao->i][hex_selecao->j]->unidade->cor.compare(color)) {
+			return 1;
+		}
+		else {
+			return 0;
+		}
+	}
+	else {
+		return 0;
+	}
+}
 
-    for(unsigned int i=0;i<hexagonos.size();i++){
-        for(unsigned int j=0;j<hexagonos[i].size();j++){
-        //cout<<"indice="<<hexagonos[i][j]->indice<<endl;
-            //if(hexagonos[i][j]->contem_unidade==1){
-                //cout<<"calculando..., indice="<<hexagonos[i][j]->indice<<endl;
-                vx = abs(x_f - hexagonos[i][j]->centro_x);
-                vy = abs(y_f - hexagonos[i][j]->centro_y);
-                re = (vx*vx)+(vy*vy);
-                re = sqrt(re);
-                    if(re <= 20){
-                        hex_selecao->i = i;
-                        hex_selecao->j = j;
-                        //cout<<"hex_selecao->i"<<hex_selecao->i <<endl;
-                        //cout<<"hex_selecao->j"<<hex_selecao->j <<endl;
-                        return 1;
-                    }
-           // }
-            /*hex_selecao->i = i;
-            hex_selecao->j = j;*/
+/* verifies the range of moving of a soldier */
+int alcance_movimento_soldado()
+{
+	if ((abs(hex_selecao->j_antes - hex_selecao->j) <= 1)
+		 &&(abs(hex_selecao->i_antes - hex_selecao -> i)  >0)
+		 &&(abs(hex_selecao->i_antes - hex_selecao->i) < 3)) {
+		if ((abs(hex_selecao->i_antes - hex_selecao->i) == 2)
+			 &&(abs(hex_selecao->j_antes - hex_selecao->j) == 1 )) {
+			return 0;
+		}
+		else {
+			/* nothing to do */
+		}
+		if (hexagonos[hex_selecao->i][hex_selecao->j]->contem_unidade) {
+			return 0;
+		}
+		else {
+			/* nothing to do */
+		}
+		return 1;
+	}
+	else {
+		return 0;
+	}
 
-        }
-
-    }
-
-    return 0;
 
 }
 
+/* verifies the range of the soldier's atack */
+int alcance_ataque_soldado()
+{
+	if ((abs(hex_selecao->j_antes - hex_selecao->j) <= 1)
+		 &&(abs(hex_selecao->i_antes - hex_selecao->i) > 0)
+		 &&(abs(hex_selecao->i_antes - hex_selecao->i) < 3)) {
+		if ((abs(hex_selecao->i_antes - hex_selecao->i) == 2)  
+			 &&(abs(hex_selecao->j_antes - hex_selecao->j) == 1 )) {
+			return 0;
+		}
+		else {
+			/* nothing to do */
+		}
 
-int possui_unidade(string color){
+		if (hexagonos[hex_selecao->i][hex_selecao->j]->contem_unidade) {
+			Unidade *temp1 = hexagonos[hex_selecao->i_antes][hex_selecao->j_antes]->unidade;
+			Unidade *temp2 = hexagonos[hex_selecao->i][hex_selecao->j]->unidade;
+			if (temp2->cor != temp1->cor) {
+				return 1;
+			}
+			else {
+				/* nothing to do */
+			}
+		}
+		else {
+			/* nothing to do */
+		}
 
-    if(hexagonos[hex_selecao->i][hex_selecao->j]->contem_unidade){
-        if(!hexagonos[hex_selecao->i][hex_selecao->j]->unidade->cor.compare(color)){
-            return 1;
-        }else{
-            return 0;
-        }
-    }
-    else{
-        return 0;
-    }
-}
-
-
-int alcance_movimento_soldado(){
-
-    if((abs(hex_selecao->j_antes - hex_selecao->j) <= 1) && (abs(hex_selecao->i_antes - hex_selecao->i)  >0) && (abs(hex_selecao->i_antes - hex_selecao->i)  <3) ){
-        if ((abs(hex_selecao->i_antes - hex_selecao->i) == 2  && (abs(hex_selecao->j_antes - hex_selecao->j) == 1 ))){
-            return 0;
-        }
-
-        if(hexagonos[hex_selecao->i][hex_selecao->j]->contem_unidade){
-            return 0;
-        }
-
-        return 1;
-    }
-    else{
-        return 0;
-    }
-
-
-}
-
-int alcance_ataque_soldado(){
-    if((abs(hex_selecao->j_antes - hex_selecao->j) <= 1) && (abs(hex_selecao->i_antes - hex_selecao->i)  >0) && (abs(hex_selecao->i_antes - hex_selecao->i)  <3) ){
-        if ((abs(hex_selecao->i_antes - hex_selecao->i) == 2  && (abs(hex_selecao->j_antes - hex_selecao->j) == 1 ))){
-            return 0;
-        }
-
-        if(hexagonos[hex_selecao->i][hex_selecao->j]->contem_unidade){
-            Unidade *temp1 = hexagonos[hex_selecao->i_antes][hex_selecao->j_antes]->unidade;
-            Unidade *temp2 = hexagonos[hex_selecao->i][hex_selecao->j]->unidade;
-            if(temp2->cor != temp1->cor)
-                return 1;
-        }
-
-        return 0;
-    }
-    else{
-        return 0;
-    }
+		return 0;
+	}
+	else {
+		return 0;
+	}
 
 }
-
-
-
-
-
-
-
-
-
